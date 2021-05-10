711113b41d3999a72409c4e77d67da85fb13209f
Namenai <36117049+bfp4f360@users.noreply.github.com>
2021-05-10 19:29:30 -0400

    bash order

--- Bash/set_pbos.bat
+++ Bash/set_pbos.bat
@@ -2,7 +2,7 @@ REM List of all PBO's
 set Arr[0]=332nd_backpacks
 set Arr[1]=332nd_helmets
 set Arr[2]=332nd_main
-set Arr[3]=332nd_units
+set Arr[3]=332nd_vehicle_weapons
 set Arr[4]=332nd_weapons
-set Arr[5]=332nd_vehicle_weapons
-set Arr[6]=332nd_vehicles
+set Arr[5]=332nd_vehicles
+set Arr[6]=332nd_units
