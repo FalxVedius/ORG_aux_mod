932e06a686c6478c6270fa7d105631d12a124e31
Jeremy <Jeremy@cates.org>
2021-05-28 18:10:07 -0400

    All helmets armor value doubled and armor setup for inventory + ace
    All helmets armor value doubled and armor setup for inventory + uniforms ace setup

--- addons/332nd_helmets/config.cpp
+++ addons/332nd_helmets/config.cpp
@@ -51,37 +51,91 @@ class CfgWeapons
 
 	class JLTS_CloneHelmetAB;
 
+	class CloneHelmetAB_Base : JLTS_CloneHelmetAB
+	{
+		class ItemInfo;
+	};
+
 	//Base airborne helmet
-	class MACRO_NEW_HELMET(airborne,base): JLTS_CloneHelmetAB
+	class MACRO_NEW_HELMET(airborne,base): CloneHelmetAB_Base
 	{
 		scopeArsenal = 0;
         scope = 0;
         author = MACRO_AUTHOR;
         displayName = "[332nd] Airborne Helmet ('base')";
         subItems[] = {"G_B_Diving"};
+
+		class ItemInfo : ItemInfo
+		{
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitpointName = "HitHead";
+					armor = 16;
+					passThrough = 0.5;
+				};
+			};
+		};
 	}
 
 	class 3as_P2_Pilot_helmet;
 
+	class CloneHelmetPilot_Base : 3as_P2_Pilot_helmet
+	{
+		class ItemInfo;
+	};
+
 	//Base aviation helmet
-	class MACRO_NEW_HELMET(aviation,base): 3as_P2_Pilot_helmet
+	class MACRO_NEW_HELMET(aviation,base): CloneHelmetPilot_Base
 	{
 		scopeArsenal = 0;
         scope = 0;
         author = MACRO_AUTHOR;
         displayName = "[332nd] Aviation Helmet ('base')";
         subItems[] = {"Integrated_NVG_TI_1_F"};
+
+		class ItemInfo : ItemInfo
+		{
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitpointName = "HitHead";
+					armor = 16;
+					passThrough = 0.5;
+				};
+			};
+		};
 	}
 
 	class 3as_Engineer_Helmet;
 
+	class CloneHelmetWarden_Base : 3as_Engineer_Helmet
+	{
+		class ItemInfo;
+	};
+
 	//Base Warden helmet
-	class MACRO_NEW_HELMET(warden,base): 3as_Engineer_Helmet
+	class MACRO_NEW_HELMET(warden,base): CloneHelmetWarden_Base
 	{
 		scopeArsenal = 0;
         scope = 0;
         author = MACRO_AUTHOR;
         displayName = "[332nd] Warden Helmet ('base')";
         subItems[] = {"Integrated_NVG_TI_1_F"};
+
+		class ItemInfo : ItemInfo
+		{
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitpointName = "HitHead";
+					armor = 16;
+					passThrough = 0.5;
+				};
+			};
+		};
 	}
 }
