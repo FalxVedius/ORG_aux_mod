dc1623edeebdad12402278502fec5fc079303ec5
Lucas <83622613+Halligan-332@users.noreply.github.com>
2021-08-08 06:54:34 +1000

    Insignia's

--- addons/332nd_units/Insignia/config.cpp
+++ addons/332nd_units/Insignia/config.cpp
@@ -25,4 +25,39 @@ class CfgUnitInsignia
 		texture = "\332nd_units\Insignia\data\Torrent1_2.paa";			// Image path
 		material = "\MRC\JLTS\Core_mod\data\insignias\insignia_CloneArmor.rvmat";	// .rvmat path
 	};
+	class 332nd_Roundel
+	{
+		displayName = "[332nd] GAR Roundel";								// Name displayed in Arsenal
+		author = "Halligan";											// Author displayed in Arsenal
+		texture = "\332nd_units\Insignia\data\332_Roundel.paa";			// Image path
+		material = "\MRC\JLTS\Core_mod\data\insignias\insignia_CloneArmor.rvmat";	// .rvmat path
+	};
+	class 332nd_Specialist_Roundel
+	{
+		displayName = "[332nd] Specialist GAR Roundel";								// Name displayed in Arsenal
+		author = "Halligan";											// Author displayed in Arsenal
+		texture = "\332nd_units\Insignia\data\Specialist_Roundel.paa";			// Image path
+		material = "\MRC\JLTS\Core_mod\data\insignias\insignia_CloneArmor.rvmat";	// .rvmat path
+	};
+	class 332nd_Ahsoka
+	{
+		displayName = "[332nd] Ahsoka Pattern";								// Name displayed in Arsenal
+		author = "Halligan";											// Author displayed in Arsenal
+		texture = "\332nd_units\Insignia\data\Ahsoka_Mark.paa";			// Image path
+		material = "\MRC\JLTS\Core_mod\data\insignias\insignia_CloneArmor.rvmat";	// .rvmat path
+	};
+	class 332nd_Reaper_Roundel
+	{
+		displayName = "[332nd] Reaper Roundel";								// Name displayed in Arsenal
+		author = "Halligan";											// Author displayed in Arsenal
+		texture = "\332nd_units\Insignia\data\Reaper_Roundel.paa";			// Image path
+		material = "\MRC\JLTS\Core_mod\data\insignias\insignia_CloneArmor.rvmat";	// .rvmat path
+	};
+	class 332nd_Seraph_Roundel
+	{
+		displayName = "[332nd] Seraph Roundel";								// Name displayed in Arsenal
+		author = "Halligan";											// Author displayed in Arsenal
+		texture = "\332nd_units\Insignia\data\Seraph_Roundel.paa";			// Image path
+		material = "\MRC\JLTS\Core_mod\data\insignias\insignia_CloneArmor.rvmat";	// .rvmat path
+	};
 };
