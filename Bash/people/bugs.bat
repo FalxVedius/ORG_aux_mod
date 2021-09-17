e8fa11c3de1eaca1fdd01a5a2e3519cf62db3c6a
Jbuges <88085487+Jbuges@users.noreply.github.com>
2021-09-16 21:00:07 -0700

    Updated Bat
    Computer back!!

--- Bash/people/bugs.bat
+++ Bash/people/bugs.bat
@@ -1,13 +1,13 @@
 call ../set_pbos.bat
 
-set outputFolder=C:\Users\Javon\Desktop\332nd_Aux_Team\@332nd_Local\addons
-set sourceFolder=C:\Users\Javon\Desktop\332nd_Aux_Team\332nd_aux_mod\addons
+set outputFolder=E:\332nd_aux_team\@332_Local\addons
+set sourceFolder=E:\332nd_aux_team\332nd_aux_mod\addons
 
 REM Folder of the addonBuilder.exe, needed so that the script can change directory to it
 set AddonBuilderPath=C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Tools\AddonBuilder
-set tempFolder=C:\Users\Javon\AppData\Local\Temp
+set tempFolder=C:\Users\jac10\AppData\Local\Temp
 
 REM Files to keep when building, like .hpp and shit
-set includeFile=C:\Users\Javon\Desktop\332nd_Aux_Team\332nd_aux_mod\Bash\include.txt
+set includeFile=E:\332nd_aux_team\332nd_aux_mod\Bash\include.txt
 
 call ../build.bat
