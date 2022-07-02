6c2a26407fbc1fc55e367d7791c17cfb5c8126a7
Jeremy Cates <Jeremy@cates.org>
2022-07-01 22:21:54 -0400

    SAC Health Fix

--- addons/332nd_vehicles/land/SAC/config.cpp
+++ addons/332nd_vehicles/land/SAC/config.cpp
@@ -48,7 +48,7 @@ class CfgVehicles
         editorSubcategory = "EdSubcat_332nd_TANK";
         crew = "332nd_aux_cisb1crew_unit_332nd_CIS_B1_Crew";
 
-        armor = 250;
-        armorStructural = 2;
+        armor = 300;
+        armorStructural = 1;
     };
 };
