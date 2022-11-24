4e5fd8baa7dc1ea787936b6e7e78168019181b73
Chaos <tetsuyahashirama@yahoo.ro>
2022-11-24 22:18:15 +0200

    First batch of texture imports

--- addons/332nd_helmets/aviation/config.cpp
+++ addons/332nd_helmets/aviation/config.cpp
@@ -27,6 +27,7 @@ class CfgPatches
 			MACRO_NEW_HELMET(aviation,Kink),
 			MACRO_NEW_HELMET(aviation,Halligan),
 			MACRO_NEW_HELMET(aviation,Pockets),
+			MACRO_NEW_HELMET(aviation,Jager),
         };
 		weapons[] = {};
 	};
@@ -54,5 +55,6 @@ class CfgWeapons
 	MACRO_NEW_AVIATION_HELMET_ITEM(Kink,Kink,Kink.paa)
 	MACRO_NEW_AVIATION_HELMET_ITEM(Halligan,Halligan,Halligan.paa)
 	MACRO_NEW_AVIATION_HELMET_ITEM(Pockets,Pockets,Pockets.paa)
+	MACRO_NEW_AVIATION_HELMET_ITEM(Jager,Jager,Jager.paa)
 	
 }
